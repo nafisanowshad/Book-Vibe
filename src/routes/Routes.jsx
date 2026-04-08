23b1082eb950c773ccbfecb4784d7679b49a4d32
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-04-08 22:15:15 +0600

    all package added and navbar styling running

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Homepage from "../pages/homepage/Homepage";
+import Books from "../pages/books/Books";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
+export const router=createBrowserRouter([
+  {
+  path: "/",
+  element: <MainLayout/>,
+  children: [
+    {
+      index: true,
+      element: <Homepage/>,
+    },
+    {
+      path: "/books",
+      element: <Books/>,
+    },
+  ],
+  errorElement: <ErrorPage/>,
+  },
+])
